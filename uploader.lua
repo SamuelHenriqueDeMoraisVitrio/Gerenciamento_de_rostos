63f878162a35205f316c2b10707271d5ff474d3c
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-26 06:40:16 -0300

    att

--- uploader.lua
+++ uploader.lua
@@ -27,17 +27,15 @@ local function mudanca()
 end
 mudanca()
 
-if false then
-    local sha_antigo = dtw.generate_sha_from_folder_by_content("src")
-    while true do
+local sha_antigo = dtw.generate_sha_from_folder_by_content("src")
+while true do
+    mudanca()
+    print("verificando ..")
+    os.execute("git pull")
+    local sha_novo = dtw.generate_sha_from_folder_by_content("src")
+    if sha_antigo ~= sha_novo then
+        sha_antigo = sha_novo
         mudanca()
-        print("verificando ..")
-        os.execute("git pull")
-        local sha_novo = dtw.generate_sha_from_folder_by_content("src")
-        if sha_antigo ~= sha_novo then
-            sha_antigo = sha_novo
-            mudanca()
-        end
-        os.execute("sleep 60")
     end
+    os.execute("sleep 60")
 end
