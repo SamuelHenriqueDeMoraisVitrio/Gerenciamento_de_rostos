1761cc9964859d9744daa2551dbb648279e3c66f
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-07-26 06:39:19 -0300

    arr

--- uploader.lua
+++ uploader.lua
@@ -14,29 +14,29 @@ local function move_src()
     end
 end
 
-
 local function mudanca()
-    print("matando servidor anterior")
-    os.execute("killall screen")
+    os.execute("screen -X -S server quit")
     print("torcendo para a porta estar livre")
     os.execute("sleep 60")
     print("movendo arquivos")
     move_src()
-    print("inicializando servidor")
-
-    os.execute("cd producao/ && screen -dm bash -c 'lua main.lua'")
+    print("iniciando novo servidor")
+    os.execute("cd producao/ && screen -S server -dm bash -c 'lua main.lua'")
     print("deve estar rodando")
 end
+mudanca()
 
-local sha_antigo = dtw.generate_sha_from_folder_by_content("src")
-while true do
-    mudanca()
-    print("verificando ..")
-    os.execute("git pull")
-    local sha_novo = dtw.generate_sha_from_folder_by_content("src")
-    if sha_antigo ~= sha_novo then
-        sha_antigo = sha_novo
+if false then
+    local sha_antigo = dtw.generate_sha_from_folder_by_content("src")
+    while true do
         mudanca()
+        print("verificando ..")
+        os.execute("git pull")
+        local sha_novo = dtw.generate_sha_from_folder_by_content("src")
+        if sha_antigo ~= sha_novo then
+            sha_antigo = sha_novo
+            mudanca()
+        end
+        os.execute("sleep 60")
     end
-    os.execute("sleep 60")
 end
