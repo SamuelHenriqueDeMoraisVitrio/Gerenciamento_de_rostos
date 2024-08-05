40dd7f4c84b98154e834ff88633a44e1ad827b4f
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-08-05 17:31:41 -0300

    Dependencias imagemagick

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,8 @@
+
+gcc -shared -fpic -o dependencies/imageMagick/imageMGK.so cBind/imageMagick/main.c -I./cBind/imageMagick/dependencies/ImageMagick-7 -L./cBind/imageMagick/dependencies/lib -fopenmp -DMAGICKCORE_HDRI_ENABLE=1 -DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_CHANNEL_MASK_DEPTH=32 -lMagickWand-7.Q16HDRI -lMagickCore-7.Q16HDRI
+
 #gcc -shared -fpic -o dependencies/imageMagick/imageMGK.so cBind/imageMagick/main.c `pkg-config --cflags --libs MagickWand`
-gcc -shared -fpic -o dependencies/dlib_face_detection/extract.so cBind/extract_lua.c
+#gcc -shared -fpic -o dependencies/dlib_face_detection/extract.so cBind/extract_lua.c
 
 #gcc -shared -fpic -o teste.so extract_lua.c
 #gcc -o imagemagick_example main.c `pkg-config --cflags --libs MagickWand`
